836397781d766d25bb43a324dd824e8501c6a8c3
grafbraga <108309912+grafbraga@users.noreply.github.com>
2025-03-01 17:15:41 +0700

    Update subtitles-sync.js

--- subtitles-sync.js
+++ subtitles-sync.js
@@ -1,8 +1,8 @@
 // ==LampaPlugin==
 // Name: Subtitles Sync
-// Description: Plugin for loading subtitles via direct .srt links from Subadub and My-Subs
-// Version: 1.0.14
-// Author: Grok3-xAI
+// Description: Plugin for auto-generating subtitles using Web Speech API
+// Version: 1.0.15
+// Author: grafbraga
 // ==/LampaPlugin==
 
 (function () {
@@ -12,79 +12,91 @@
 
     var SubtitlesSync = {
         name: 'SubtitlesSync',
-        version: '1.0.14',
-        sources: {
-            'Subadub': 'https://subadub.app',
-            'My-Subs': 'https://my-subs.co'
-        },
-        defaultSource: 'My-Subs',
-        languages: ['en', 'ru', 'es', 'fr', 'de'],
-        selectedLang: 'en',
-        selectedSource: 'My-Subs',
+        version: '1.0.15',
+        recognition: null,
+        subtitles: [],
 
         init: function () {
             var _this = this;
+
+            // Инициализация Web Speech API
+            if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
+                var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+                this.recognition = new SpeechRecognition();
+                this.recognition.continuous = true;
+                this.recognition.interimResults = true;
+                this.recognition.lang = 'en-US'; // По умолчанию английский, можно изменить в будущем
+                
+                this.recognition.onresult = function (event) {
+                    var transcript = '';
+                    for (var i = event.resultIndex; i < event.results.length; i++) {
+                        transcript += event.results[i][0].transcript;
+                    }
+                    _this.addSubtitle(transcript);
+                };
+
+                this.recognition.onerror = function (event) {
+                    Lampa.Noty.show('Speech recognition error: ' + event.error);
+                };
+            } else {
+                Lampa.Noty.show('Web Speech API not supported in this browser');
+                return;
+            }
+
+            // Отложенная инициализация после готовности приложения
             Lampa.Listener.follow('app', function (e) {
                 if (e.type == 'ready') {
                     setTimeout(function () {
-                        if (Lampa.Noty) Lampa.Noty.show('Subtitles Sync: App ready');
-                        _this.addSettings();
+                        _this.setupPlayer();
                     }, 500);
                 }
             });
         },
 
-        addSettings: function () {
+        setupPlayer: function () {
             var _this = this;
 
-            // Добавление в меню настроек
-            Lampa.SettingsApi.addParam({
-                component: 'interface',
-                param: {
-                    name: 'subtitles_sync_source',
-                    type: 'select',
-                    values: this.sources,
-                    default: this.defaultSource
-                },
-                field: 'Источник субтитров',
-                onChange: function (value) {
-                    _this.selectedSource = value;
-                    Lampa.Storage.set('subtitles_sync_source', value);
-                }
-            });
-
-            Lampa.SettingsApi.addParam({
-                component: 'interface',
-                param: {
-                    name: 'subtitles_sync_lang',
-                    type: 'select',
-                    values: this.languages.reduce(function (result, lang) {
-                        result[lang] = lang.toUpperCase();
-                        return result;
-                    }, {}),
-                    default: this.selectedLang
-                },
-                field: 'Язык субтитров',
-                onChange: function (value) {
-                    _this.selectedLang = value;
-                    Lampa.Storage.set('subtitles_sync_lang', value);
-                }
-            });
-
-            this.selectedSource = Lampa.Storage.get('subtitles_sync_source', this.defaultSource);
-            this.selectedLang = Lampa.Storage.get('subtitles_sync_lang', this.selectedLang);
-
-            // Добавление в меню плеера
+            // Добавление пункта в меню плеера
             if (Lampa.PlayerMenu) {
                 Lampa.PlayerMenu.add({
                     title: 'Subtitles Sync',
-                    subtitle: 'Load subtitles via direct links',
+                    subtitle: 'Generate subtitles automatically',
                     icon: 'subtitles',
                     action: function () {
-                        Lampa.Noty.show('Subtitles Sync: Menu clicked');
+                        _this.startRecognition();
                     }
                 });
             }
+
+            Lampa.Listener.follow('player', function (e) {
+                if (e.type == 'start') {
+                    _this.subtitles = []; // Очистка субтитров при новом видео
+                }
+            });
+        },
+
+        startRecognition: function () {
+            if (this.recognition) {
+                this.subtitles = [];
+                this.recognition.start();
+                Lampa.Noty.show('Started generating subtitles...');
+            }
+        },
+
+        addSubtitle: function (text) {
+            var player = Lampa.Player;
+            var currentTime = player.time();
+
+            this.subtitles.push({
+                start: currentTime,
+                end: currentTime + 2, // Предполагаемая длительность 2 секунды
+                text: text
+            });
+
+            player.subtitles.add({
+                label: 'Auto-Generated',
+                content: this.subtitles
+            });
         }
     };
 
